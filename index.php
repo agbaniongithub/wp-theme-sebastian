3708151b7e6348784829e00c5dfeded274ecae98
Eric Thomas <fiyin.agba@gmail.com>
2024-06-16 08:16:31 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Main template file for Sebastian Theme by David Agbaniyaka.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sebastian</title>
+</head>
+<body>
+  
+</body>
+</html>
